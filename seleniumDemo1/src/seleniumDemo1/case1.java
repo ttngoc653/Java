2aabe84078a1bfd1c015c25e682a345942855e5e
Yount Zitch <Yount Zitch@PC20180221>
2018-02-23 09:44:19 +0700

    selenium up to git demo will delete

---
+++ seleniumDemo1/src/seleniumDemo1/case1.java
@@ -0,0 +1,20 @@
+package seleniumDemo1;
+
+public class case1 {
+
+	public static void main(String[] args) {
+		/*System.setProperty("webdriver.opera.driver","operadriver.exe");
+		OperaOptions options=new OperaOptions();
+		options.setBinary("C:\\Program Files\\Opera\\launcher.exe");
+		WebDriver driver=new OperaDriver(options);
+		driver.get("http://themini6.webitop.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+		driver.quit();
+		System.out.println("End!!!");
+		*/
+		System.out.println((Math.random()*Math.pow(10, 10)));
+		
+	}
+
+}
